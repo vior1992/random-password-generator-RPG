d93ddb19c728870b178ffceb5b61d924b81e2ed2
Dani Villegas <dani@Danis-MacBook-Pro.local>
2019-08-05 17:37:56 +0300

    changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 TODO:
 
-pass length opcional (fix min and max)
+
 Styles
 test
 add cool font
-Add message 5 delay when copied password
+imports
 readme
--- scripts/logic.js
+++ scripts/logic.js
@@ -33,6 +33,7 @@ const logic = {
 
     passwordLengthValidator(passLength) {
         if (passLength < 8 || passLength > 32) return 'Incorrect length, enter a length betheen 8 and 32 characters';
+        if (!passLength.trim().length) return "Length is empty or blank";
         if (isNaN(passLength)) return 'Lenght is not a number';
         return false;
     }
