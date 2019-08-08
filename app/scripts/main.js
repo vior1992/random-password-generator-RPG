ff7b00eae771edd457bacc56b09d4236a9d5bd2d
Dani Villegas <dani@Danis-MacBook-Pro.local>
2019-08-08 15:59:30 +0300

    add styles & schafolder apropiatly & passValidator on logic

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,32 +2,32 @@
 
 const form = document.getElementById('options__form');
 const passwordLength = document.getElementById('length__input');
-const password = document.getElementById('pass__container');
 document.addEventListener('click', checkClick);
 
-window.onload = () => {    
+window.onload = () => {
     const OPTION_TYPES = {
         lower_case: 'abcdefghijklmnopqrstuvwxyz',
         upper_case: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
         numbers: '0123456789',
         special_symbols: '!$%^&*()-=+[]{};#:@~,./<>?'
     };
-    
+
     Object.keys(OPTION_TYPES).map(option => form.appendChild(view.passwordOptions(option)));
 };
 
-function checkClick({ target: { id } }) { 
+function checkClick({ target: { id } }) {
     switch (id) {
         case 'submit__button':
+            const mainSection = document.getElementById('main__section');
             const hasPassword = document.getElementById('p__password');
-            const hasError = document.getElementById('error');    
- 
-            if (hasPassword) document.getElementById('p__password').remove();
+            const hasError = document.getElementById('error');
+
+            if (hasPassword) document.getElementById('pass__container').remove();
             if (hasError) document.getElementById('error').remove();
 
             const { value } = passwordLength;
             const newPassword = logic.generatePassword(value);
-            if (newPassword) password.appendChild(view.password(newPassword));
+            if (newPassword) mainSection.appendChild(view.password(newPassword));
             break;
         case 'button__copy':
             view.copyPasswordOnClipboard();
