9e8485cb6a52d958a335be0ab7ea4f1f5420fa21
Dani Villegas <dani@Danis-MacBook-Pro.local>
2019-08-09 11:50:38 +0300

    fix bug special symbol ">", changes on styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,39 @@
-// import OPTION_TYPES from '../constants';
+// const OPTION_TYPES = require('../constants');
+// const Password = require('./logic');
 
+const password = new Password();
 const form = document.getElementById('options__form');
 const passwordLength = document.getElementById('length__input');
 document.addEventListener('click', checkClick);
 
 window.onload = () => {
-    const OPTION_TYPES = {
-        lower_case: 'abcdefghijklmnopqrstuvwxyz',
-        upper_case: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        numbers: '0123456789',
-        special_symbols: '!$%^&*()-=+[]{};#:@~,./<>?'
-    };
-
+  const OPTION_TYPES = {
+    lower_case: 'abcdefghijklmnopqrstuvwxyz',
+    upper_case: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numbers: '0123456789',
+    special_symbols: '!$%^&*()-=+[]{};#:@~,./<>?'
+  };
     Object.keys(OPTION_TYPES).map(option => form.appendChild(view.passwordOptions(option)));
 };
 
 function checkClick({ target: { id } }) {
-    switch (id) {
-        case 'submit__button':
-            const mainSection = document.getElementById('main__section');
-            const hasPassword = document.getElementById('p__password');
-            const hasError = document.getElementById('error');
+  switch (id) {
+    case 'submit__button':
+      const mainSection = document.getElementById('main__section');
+      const hasPassword = document.getElementById('p__password');
+      const hasError = document.getElementById('error');
 
-            if (hasPassword) document.getElementById('pass__container').remove();
-            if (hasError) document.getElementById('error').remove();
+      if (hasPassword) document.getElementById('pass__container').remove();
+      if (hasError) document.getElementById('error').remove();
 
-            const { value } = passwordLength;
-            const newPassword = logic.generatePassword(value);
-            if (newPassword) mainSection.appendChild(view.password(newPassword));
-            break;
-        case 'button__copy':
-            view.copyPasswordOnClipboard();
-            break;
-        default: null;
-            break;
-    };
+      const { value } = passwordLength;
+      const newPassword = password.generatePassword(value);
+      if (newPassword) mainSection.appendChild(view.password(newPassword));
+      break;
+    case 'button__copy':
+      view.copyPasswordOnClipboard();
+      break;
+    default: null;
+      break;
+  };
 };
